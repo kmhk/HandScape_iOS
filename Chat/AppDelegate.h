8bc9c7f9761a77e8366e1d85ead3bc76d27d04ab
Nikolay Petrovich <goldentime@nikolays-mac.local>
2016-04-12 21:02:55 -0700

    Initial Commit

---
+++ Chat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Chat
+//
+//  Created by Nikolay Petrovich on 4/12/16.
+//  Copyright © 2016 handscape.chat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Chat/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Chat
+//
+//  Created by Nikolay Petrovich on 4/12/16.
+//  Copyright © 2016 handscape.chat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
